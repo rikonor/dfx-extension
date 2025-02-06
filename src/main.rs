60927b8f52c947910297c0a82a47171060c3fc1b
Or Ricon <rikonor@gmail.com>
2025-02-06 18:57:35 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::path::PathBuf;
+
+use anyhow::Error;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+
+    /// Path to dfx cache
+    #[arg(long("dfx-cache-path"), env = "DFX_CACHE_PATH", global = true)]
+    cache: Option<PathBuf>,
+}
+
+#[derive(Parser)]
+enum SubCommand {
+    Ping,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let _cli = Cli::parse();
+
+    Ok(())
+}
